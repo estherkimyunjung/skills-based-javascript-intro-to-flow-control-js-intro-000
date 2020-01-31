6cd432b92ae03498fde767b33805c0ae09d6deae
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-01-31 17:08:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,24 +1,24 @@
 function basicTeenager(age) {
   if(age >= 13 && age <= 19) {
-    console.log("You are a teenager!");
+    return ("You are a teenager!");
   }
 }
 
 function teenager(age) {
   if(age >= 13 && age <= 19) {
-    console.log("You are a teenager!");
+    return ("You are a teenager!");
   } else {
-    console.log("You are not a teenager");
+    return ("You are not a teenager");
   }
 }
 
 function ageChecker(age) {
   if(age >= 13 && age <= 19) {
-    console.log("You are a teenager!");
+    return ("You are a teenager!");
   } else if(age <= 12) {
-    console.log("You are a kid");
+    return ("You are a kid");
   } else {
-    console.log("You are a grownup");
+    return ("You are a grownup");
   }
 }
 
@@ -34,3 +34,4 @@ function switchAge(age) {
 basicTeenager(15);
 teenager(20);
 ageChecker(30);
+ternaryTeenager(19);
